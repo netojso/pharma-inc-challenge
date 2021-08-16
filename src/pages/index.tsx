aa3856f08ef92adadeaf712b6ae30e8cff235ff2
netojso <neto.devx@gmail.com>
2021-08-16 16:07:04 -0300

    refactor: some layout changes and clean code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 /* eslint-disable react/no-unused-prop-types */
 import React, { useState, useEffect } from 'react';
 
-import { PlusIcon, RefreshIcon } from '@heroicons/react/solid';
 import { GetServerSideProps } from 'next';
 import router, { useRouter } from 'next/router';
 
-import { LogoSVG } from '../assets/Logo';
-import { Modal, SearchInput } from '../components';
-import { Table } from '../components/Table';
+import { Modal, SearchInput, Table } from '../components';
 import { api, getParams } from '../services/api';
 import { User } from '../types/User';
 import { formatUserData } from '../utils/formatResponseApiData';
@@ -21,6 +18,7 @@ export default function Home({ users: usersApi }: IMainProps) {
   const [showModal, setShowModal] = useState<boolean>(false);
   const [users, setUsers] = useState<User[]>(usersApi);
   const [selectedUser, setSelectedUser] = useState<User>();
+  const [searchedUsers, setSearchedUsers] = useState<User[] | null>();
   const [selectedGender, setSelectedGender] = useState<string | null>();
   const [selectedNatio, setSelectedNatio] = useState<string | null>();
   const [isLoadingMore, setIsLoadingMore] = useState<boolean>(false);
@@ -28,7 +26,7 @@ export default function Home({ users: usersApi }: IMainProps) {
   const page = useRouter().query.page as string;
 
   useEffect(() => {
-    if (selectedNatio || selectedGender) {
+    if (selectedGender || selectedNatio) {
       const urlParams = getParams(Number(page) || 1, {
         gender: selectedGender,
         natio: selectedNatio,
@@ -49,11 +47,11 @@ export default function Home({ users: usersApi }: IMainProps) {
   };
 
   const handleSearchUser = (value: string): void => {
-    if (!value) setUsers(usersApi);
-    const filtered = usersApi.filter((user) =>
+    if (!value) setSearchedUsers(null);
+    const filtered = users.filter((user) =>
       user.name.first.toLowerCase().includes(value.toLowerCase())
     );
-    setUsers(filtered);
+    setSearchedUsers(filtered);
   };
 
   const handleNextPage = async (): Promise<void> => {
@@ -83,15 +81,9 @@ export default function Home({ users: usersApi }: IMainProps) {
           <header className="text-gray-600 body-font bg-white w-full">
             <div className="container mx-auto flex flex-wrap p-3 px-5 flex-col md:flex-row items-center">
               <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 no-underline">
-                <LogoSVG />
+                <img src="/logo.png" alt="Logo" width="40px" />
                 <span className="ml-3 text-xl">Pharma Inc</span>
               </div>
-              <button
-                type="button"
-                className="inline-flex ml-auto items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
-              >
-                Button
-              </button>
             </div>
           </header>
           <SearchInput
@@ -101,43 +93,23 @@ export default function Home({ users: usersApi }: IMainProps) {
           <div className="col-span-12  ">
             <div className="overflow-y-auto" style={{ maxHeight: '72vh' }}>
               <Table
-                users={users}
+                users={searchedUsers || users}
+                isLoadingMore={isLoadingMore}
+                handleNextPage={handleNextPage}
                 handleModal={handleModal}
-                handleSelectGender={setSelectedGender}
+                handleFilterByGender={setSelectedGender}
               />
-
-              {users.length > 0 ? (
-                <button
-                  className="btn flex items-center justify-center text-white w-full"
-                  type="button"
-                  onClick={() => handleNextPage()}
-                >
-                  {isLoadingMore ? (
-                    <>
-                      <RefreshIcon className="text-gray-200" width={18} />
-                      <span className="ml-1 text-gray-200">Loading</span>
-                    </>
-                  ) : (
-                    <>
-                      <PlusIcon className="text-gray-200" width={18} />
-                      <span className="ml-1 text-gray-200">Load more</span>
-                    </>
-                  )}
-                </button>
-              ) : (
-                <p className="text-center text-white">
-                  Nenhum usuário encontrado
-                </p>
-              )}
             </div>
           </div>
         </div>
       </div>
-      <Modal
-        user={selectedUser}
-        showModal={showModal}
-        setShowModal={handleModal}
-      />
+      {selectedUser && (
+        <Modal
+          user={selectedUser}
+          showModal={showModal}
+          handleModal={handleModal}
+        />
+      )}
     </div>
   );
 }
