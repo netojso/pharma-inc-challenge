d3d5950e95afc11894df68ea571fee4f837224b1
netojso <neto.devx@gmail.com>
2021-08-15 22:59:53 -0300

    refactor: change url api and add function to get params

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,25 @@
 import axios from 'axios';
 
+export const getParams = (
+  page: number,
+  other?: {
+    gender: string | null | undefined;
+    natio: string | null | undefined;
+  }
+) => {
+  let params = `?page=${page}&results=50`;
+
+  if (other) {
+    const { gender, natio } = other;
+    if (gender) params += `&gender=${gender}`;
+    if (natio) params += `&nat=${natio}`;
+  } else {
+    params += '&seed=foobar';
+  }
+
+  return params;
+};
+
 export const api = axios.create({
-  baseURL: 'https://randomuser.me/api/?results=50&seed=foobar',
+  baseURL: 'https://randomuser.me/api/',
 });
